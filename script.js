7deb44ee0a36e2e29bafef643ec61a1082756dc1
Sabin Ghimire <sabinghimire223@gmail.com>
2025-07-05 16:26:25 +0545

    Script added

--- script.js
+++ script.js
@@ -1,3 +1,66 @@
+// Varibles
 const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
-const items = [];
+let items = JSON.parse(localStorage.getItem("Item")) || [];
+const clr = document.querySelector(".clear");
+const del = document.querySelector(".Delete");
+
+//Functions
+function addItem(e) {
+  e.preventDefault();
+  const text = this.querySelector("[name=item]").value;
+  const item = {
+    text,
+    done: false,
+  };
+  items.push(item);
+  applyList(items, itemsList);
+  localStorage.setItem("Item", JSON.stringify(items));
+  //   console.log(item);
+  this.reset();
+}
+
+function applyList(datas = [], datalist) {
+  datalist.innerHTML = datas
+    .map((data, i) => {
+      return `
+    <li>
+    <input type="checkbox" data-index=${i} id="item${i}" ${
+        data.done ? "checked" : ""
+      }/>
+    <label for="item${i}">${data.text}<label>               
+    </li>
+    `;
+    })
+    .join("");
+}
+
+function toggleDone(e) {
+  if (!e.target.matches("input")) return;
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem("Item", JSON.stringify(items));
+  applyList(items, itemsList);
+}
+function ClearAll() {
+  items.forEach((e) => {
+    e.done = false;
+
+    localStorage.setItem("Item", JSON.stringify(items));
+    applyList(items, itemsList);
+  });
+}
+function DeleteAll() {
+  items = [];
+  localStorage.setItem("Item", JSON.stringify(items));
+  applyList(items, itemsList);
+}
+applyList(items, itemsList);
+// localStorage.clear();
+
+//Event listners
+addItems.addEventListener("submit", addItem);
+itemsList.addEventListener("click", toggleDone);
+clr.addEventListener("click", ClearAll);
+del.addEventListener("click", DeleteAll);
